e88cb20b5184841c8cc8cf9bbe218e78fd762491
Jason Hilton <jasface901@gmail.com>
2017-05-06 11:27:29 -0700

    Made id render

--- Players/Enemies/IdName.gd
+++ Players/Enemies/IdName.gd
@@ -5,7 +5,7 @@ extends Label
 # var b = "textvar"
 
 func _ready():
-	set_text("hello")
+	set_text(get_node("../").id)
 	# Called every time the node is added to the scene.
 	# Initialization here
 	pass
