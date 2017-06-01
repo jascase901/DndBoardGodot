bc26a10c1d06b8e2234904682f8344a680637b76
Jason Hilton <jasface901@gmail.com>
2017-06-01 01:54:17 -0700

    Moved non physics code out of fixed_processs

--- Players/DragButton.gd
+++ Players/DragButton.gd
@@ -7,7 +7,7 @@ var game_state
 export var id="1"
 
 var is_held = false
-
+var is_this_node_selected = false
 func _ready():
 	set_material(shader.duplicate())
 	get_material().set_shader_param("enabled", false);
@@ -16,18 +16,14 @@ func _ready():
 	game_state.addFigurine(id, get_pos().x, get_pos().y);
 	set_process(true)
 	set_fixed_process(true)
-
-func _fixed_process(delta):
+	
+func _process(delta):
 	var fig = game_state.getFigurine(id)
 	if fig == null:
 		print("not in game state fig_name:", id)
 		return
 	set_pos(Vector2(fig.x, fig.y))
-	#var is_nothing_selected = SELECTED_SINGLETON.SELECTED_ID == null;
 	var mouse_pos = (get_viewport().get_mouse_pos())
-
-	var is_this_node_selected =( 
-		self.get_instance_ID() == SELECTED_SINGLETON.get_selected(mouse_pos))
 	
 	if not is_this_node_selected:
 		get_material().set_shader_param("enabled", false);	
@@ -36,8 +32,13 @@ func _fixed_process(delta):
 	if is_held:
 		set_pos(Vector2(int(mouse_pos.x), int(mouse_pos.y)))
 		get_material().set_shader_param("enabled", true);
-
-
+		
+func _fixed_process(delta):
+	#This uses RayCasting under the hood so it must happen in fixed_processes
+	var mouse_pos = (get_viewport().get_mouse_pos())
+	is_this_node_selected =( 
+		self.get_instance_ID() == SELECTED_SINGLETON.get_selected(mouse_pos))
+	
 func _on_DragableRB_input_event( viewport, event, shape_idx ):
 	if event.is_action_pressed("MOUSE_BUTTON"):
 		SELECTED_SINGLETON.SELECTED_ID=null
